04063362a3805e8a16120ca602e4e8f00524b683
HyeongSeok Kim <harry121500@naver.com>
2025-05-21 19:11:24 +0900

    change str.replace()

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def train_file_list_to_json(english_file_list: List[str], german_file_list: List
     # Preprocess unwanted characters
     def process_file(file):
         if '\\' in file:
-            file = file.replace('\\', '\\')
+            file = file.replace('\\', '\\\\')
         if '/' or '"' in file:
             file = file.replace('/', '\\/')
             file = file.replace('"', '\\"')
